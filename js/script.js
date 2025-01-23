485d41ba5ae6fa1b026a5b58b6206262a25125c9
Keiili <145891671+Keiili@users.noreply.github.com>
2025-01-23 15:03:51 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-    alert("Welcome to my website!");
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//     alert("Welcome to my website!");
+// });
